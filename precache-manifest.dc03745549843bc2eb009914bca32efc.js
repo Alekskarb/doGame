d0a173ec35b668996a25e2b67eac0da25dd0afe6
Alekskarb <korbutowski@gmail.com>
2020-02-12 10:56:09 +0300

    Updates

---
+++ precache-manifest.dc03745549843bc2eb009914bca32efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6f09dca1ef487096fb718d7e7f8b34",
+    "url": "/doGame/index.html"
+  },
+  {
+    "revision": "2c2fac8d3eb443582893",
+    "url": "/doGame/static/css/main.58e59ba5.chunk.css"
+  },
+  {
+    "revision": "394f04b087713684e833",
+    "url": "/doGame/static/js/2.3ccefaab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/doGame/static/js/2.3ccefaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2fac8d3eb443582893",
+    "url": "/doGame/static/js/main.4031d061.chunk.js"
+  },
+  {
+    "revision": "14f121c23e187f475e7f",
+    "url": "/doGame/static/js/runtime-main.520e1f3c.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/doGame/static/media/dog.75fa68d4.mp3"
+  }
+]);
