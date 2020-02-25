869104ae63d2892c8ffc3a76979b95b855f69c03
Alekskarb <korbutowski@gmail.com>
2020-02-25 12:00:28 +0300

    react-redux

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
         this.setState({count: this.state.count + 1})
     };
 
-    items = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    items = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     render = () => {
 
         return (
